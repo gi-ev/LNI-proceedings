6252b917c27ac8b72b870d16a2ba58094d6fd2ea
esdkrwl <genja007@gmx.de>
2017-08-14 15:39:37 +0200

    Create make-procedings.sh
    shell version of make-procedings.bat

---
+++ make-procedings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pdflatex -synctex=1 proceedings
+pdflatex -synctex=1 proceedings
+#TOC should be correct now
+sed -i "s/\\\\IeC //g" proceedings.bib
+biber proceedings
+pdflatex -synctex=1 proceedings
+texindy proceedings.idx
+pdflatex -synctex=1 proceedings
+pdflatex -synctex=1 proceedings
+sed -i "s/\\\\IeC //g" proceedings.bib
+sed -i "s/\\\\textunderscore //g" proceedings.bib
+sed -i "s/\\\\IeC //g" proceedings.csv
+sed -i "s/ ;/;/g" proceedings.csv
