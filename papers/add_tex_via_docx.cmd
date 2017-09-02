3ff59d2ef93aa2262be899045c5c9b7327c5d218
Oliver Kopp <kopp.dev@gmail.com>
2017-09-02 12:50:29 +0200

    Remove wrong comment

--- papers/add_tex_via_docx.cmd
+++ papers/add_tex_via_docx.cmd
@@ -1,7 +1,5 @@
 @echo off %debug%
 
-:: renaming all pdfs and texs to paper.*
-:: NOTE: all directories should contain only one .pdf and one .tex
 for /D %%i in (*) do (
 	cd "%%i"
 	for %%j in (*.docx) do (
