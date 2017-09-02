6f5ac2c070a4b48bf17cc5aa9ec5b8ddde269b79
Oliver Kopp <kopp.dev@gmail.com>
2017-09-03 00:21:23 +0200

    Fix typo

--- cropproceedings.sh
+++ cropproceedings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 perl `kpsewhich -var-value TEXMFDIST`/scripts/pax/pdfannotextractor.pl proceedings.pdf
-pdflatex proceedings-cropped.pdf
-pdflatex proceedings-cropped.pdf
+pdflatex proceedings-cropped
+pdflatex proceedings-cropped
